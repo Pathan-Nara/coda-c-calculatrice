ca07fdf8f8dbbceace118833553d3e918aff1396
Nara <nathan.para@coda-student.school>
2024-09-29 14:23:09 +0200

    etape 4 : créer les conditions

--- main.c
+++ main.c
@@ -7,9 +7,51 @@ int main()
 	int nb1;
 	int nb2;
 	char symb;
+	int fin = 0;
+
 	printf("Bonjour et bienvenu dans mon incroyable calculatrice \n");
-	printf("Choisissez l'opération que vous souhaitez effectuer \n");
-	printf("(vous avez le choix entre '+', '-', '*', '/', '%%') \n");
-	scanf("%s", &symb);
+	while(fin == 0)
+	{
+		printf("Choisissez l'opération que vous souhaitez effectuer \n");
+		printf("(vous avez le choix entre '+', '-', '*', '/', '%%') \n");
+		scanf("%c", &symb);
+
+		if(symb == '+')
+		{
+			printf("addition \n");
+			fin = 1;
+		}
+
+		else if(symb == '-')
+		{
+			printf("soustraction \n");
+			fin = 1;
+		}
+
+		else if(symb == '*')
+		{
+			printf("multiplication \n");
+			fin = 1;
+		}
+
+		else if(symb == '/')
+		{
+			printf("division \n");
+			fin = 1;
+		}
+
+		else if(symb == '%')
+		{
+			printf("modulo \n");
+			fin = 1;
+		}
+
+		else
+		{
+			printf("le caractère n'est pas reconnue veuillez réessayer. \n");
+			printf(" -------------------------------------------- \n");
+
+		}
+	}
 	exit(0);
 }
