f70a0e35746d800aa2c82108d4bb7160e0c25c31
Nara <nathan.para@coda-student.school>
2024-09-29 13:57:42 +0200

    etape 3 : recupérer les infos

--- main.c
+++ main.c
@@ -7,6 +7,9 @@ int main()
 	int nb1;
 	int nb2;
 	char symb;
-
+	printf("Bonjour et bienvenu dans mon incroyable calculatrice \n");
+	printf("Choisissez l'opération que vous souhaitez effectuer \n");
+	printf("(vous avez le choix entre '+', '-', '*', '/', '%%') \n");
+	scanf("%s", &symb);
 	exit(0);
 }
